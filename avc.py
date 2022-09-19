b00fc40709951a9dabf18613d4e305670fc17876
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-09-18 22:05:57 -0400

    testing by year - core

--- avc.py
+++ avc.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 from fpdf import FPDF
+from datetime import date
 
+
+currYear = date.today().year
 # Webpage connection
-html = "https://avc.com/category/web3"
-r=requests.get(html)
+# YEAR = '2003'
+# html = "https://avc.com/2003"
+
+baseLink = "https://avc.com/"
+for k in range(2003,currYear,1):
+    link = baseLink + str(k)
+    print(link)
+
+r=requests.get(link)
 c=r.content
 soup=BeautifulSoup(c,"html.parser")
 pdf = FPDF()
@@ -17,8 +27,8 @@ def scrap_main(wegoList):
         content = items.find_all("div", {"class": "ContentArea",})
         # date = items.find_all("div", {"class": "text-content short-text",})
         for i in zip(title,content):
-            final = print(i[0].get_text().strip(),('\n\n'),i[1].get_text().strip(),('\n\n'))
-            pdf.final("final.pdf")
+            print(i[0].get_text().strip(),('\n\n'),i[1].get_text().strip(),('\n\n'))
+            # pdf.final("final.pdf")
 
 scrap_main(wegoList)
 
@@ -32,40 +42,3 @@ scrap_main(wegoList)
 #     print(findolder)
 # except:
 #     pass
-# movie_name = results.find('h3').find('a')
-# movie_year = results.find('h3').find('span')
-# #use the strip function to trim any extra spaces or lines
-# print(movie_name.text.strip() +movie_year.text.strip()) 
-# main_content = results.find('div', class_='lister')
-# noOfReviews = main_content.find('span')
-# print(noOfReviews.text)
-
-# #find all function will return the list of all those divs, we will iterate each item to extract data from nested tags.
-
-# reviewsList = main_content.find_all('div', class_='lister-item')
-# print(reviewsList)
-
-# for review in reviewsList:
-#      #just for adding a line 
-#     print(" ")  
-#     #The div contains the user reviews
-#     listContent = review.find('div', class_='lister-item-content')
-#     reviewTitle = listContent.find('a', class_='title')
-#     print('Review Title:', reviewTitle.text.strip())
-#     rating = listContent.find('span', class_='rating-other-user-rating')  #rating span
-#     #To avoid NoneType objects
-#     if rating==None:
-#         continue
-#     #the actual rating text
-#     ratingText = rating.find_all('span')  
-#     print("Rating: ",ratingText[0].text.strip())
-#     userName = listContent.find('span', class_='display-name-link')
-#     print('Username: ', userName.text)
-#     reviewDate = listContent.find('span', class_='review-date')
-#     print('Review Date:', reviewDate.text)
-#     reviewContent = listContent.find('div', class_='text')
-#     print('Review:',reviewContent.text)
-
-
-# with open("output1.html", "w", encoding='utf-8') as reviewsList:
-#     reviewsList.write(str(soup))
--- scrap-urls.py
+++ scrap-urls.py
@@ -4,5 +4,10 @@ url = "https://avc.com/archive/"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 # print(soup.title)
-for link in soup.find_all('a'):
-    print(link.get('href'))
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
+
+baseLink = "https://avc.com/"
+for k in range(2003,2022,1):
+    link = baseLink + str(k)
+    print(link)
